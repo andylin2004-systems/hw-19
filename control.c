c550e5a81faa72965336580d7f29285a3e3adf6a
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-09 19:06:59 -0500

    19 done and some annotations as well

--- control.c
+++ control.c
@@ -11,34 +11,21 @@ int main()
     mkfifo("luigi", 0644);
     while (1)
     {
+        // read from standard in and sends it via mario pipe to worker
         char in[1024] = {0};
-        int toWorker = open("mario", O_WRONLY | O_TRUNC, 0644);
+        int toWorker = open("mario", O_WRONLY);
         printf("Enter something for the child to destroy:\n");
         read(STDIN_FILENO, in, sizeof(char) * 1023);
         write(toWorker, in, strlen(in) * sizeof(char));
+        close(toWorker);
 
-        int fromWorker = open("luigi", O_RDONLY, 0644);
+        // read result from luigi pipe and prints result
+        int fromWorker = open("luigi", O_RDONLY);
         char result[1024] = {0};
         read(fromWorker, result, sizeof(result));
 
         printf("Response: %s\n", result);
 
-        //     char toEdit[1024] = {0};
-        //     read(pipes[0], toEdit, sizeof(toEdit));
-        //     close(pipes[0]);
-        //     int i;
-        //     for (i = 0; i < 1024 && toEdit[i]; i++){
-        //         toEdit[i] += 1;
-        //         if (toEdit[i] == 'a' || toEdit[i] == 'A'){
-        //             toEdit[i] = '4';
-        //         }else if (toEdit[i] == 'e' || toEdit[i] == 'E'){
-        //             toEdit[i] = '3';
-        //         }else if (toEdit[i] == 'I' || toEdit[i] == 'i'){
-        //             toEdit[i] = '1';
-        //         }
-        //     }
-        //     write(pipes[3], toEdit, sizeof(toEdit));
-        //     return 0;
-        // }
+        close(fromWorker);
     }
 }
--- worker.c
+++ worker.c
@@ -6,10 +6,13 @@
 int main()
 {
     while (1)
-    {
-        int fromControl = open("mario", O_WRONLY | O_TRUNC, 0644);
+    {   
+        // read from mario pipe and processes what went through
+        int fromControl = open("mario", O_RDONLY);
         char toEdit[1024] = {0};
         read(fromControl, toEdit, sizeof(toEdit));
+        close(fromControl);
+        
         int i;
         for (i = 0; i < 1024 && toEdit[i]; i++)
         {
@@ -27,7 +30,10 @@ int main()
                 toEdit[i] = '1';
             }
         }
-        int toControl = open("luigi", O_RDONLY, 0644);
+
+        // write the manipulated string and sends it to luigi pipe and control
+        int toControl = open("luigi", O_WRONLY);
         write(toControl, toEdit, sizeof(toEdit));
+        close(toControl);
     }
 }
